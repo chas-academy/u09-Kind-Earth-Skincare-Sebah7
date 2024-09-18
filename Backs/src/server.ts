a23fecb4921e384fbf0a71a7b87dc03fccd55025
Sebah7 <sebahismail21@gmail.com>
2024-09-18 10:19:33 +0200

    lil fix

--- Backs/src/server.ts
+++ Backs/src/server.ts
@@ -5,8 +5,10 @@ import app from "./app";
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-connectKindEarthDB();
-
-app.listen(port, () => {
+connectKindEarthDB().then(() => {
+  app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
 });
