265b6e5fd146d03b81463b85396a88f5ca41c5a1
Sebah7 <sebahismail21@gmail.com>
2024-09-19 13:08:52 +0200

    App cleaned - kinda, icon and login img

--- Fronts/src/App.tsx
+++ Fronts/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import Search from './components/Shared/SearchBar.tsx'
+import Login from './Pages/LoginPage.tsx'
+// import Register from './Pages/SignupPage.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     {/* <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+        <h1 className="text-2xl font-bold text-center">Hello, Tailwind CSS!</h1>
+        <p className="text-gray-700 text-center">This is a test component to verify Tailwind CSS is working.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div> */}
+
+<Login />
     </>
   )
 }
