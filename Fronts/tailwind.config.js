c64209c6dfaad51cb4c63392f32dce89486f8d2b
Sebah7 <sebahismail21@gmail.com>
2024-09-19 13:07:56 +0200

    dependencies and pacakges installed and index.css fixed

--- Fronts/tailwind.config.js
+++ Fronts/tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
+       "./index.html", 
+       "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+black: "#000",
+gray: "rgba(30, 30, 30, 0.72)"
+},
+
+fontFamily: {
+sans: ["Dancing Script", "cursive", "Helvetica", "Arial", "sans-serif"],
+},
+fontSize: {
+inherit: "inherit"
+},
+ },
+},
+corePlugins: {
+preflight: false
+},
   plugins: [],
 }
+
 
