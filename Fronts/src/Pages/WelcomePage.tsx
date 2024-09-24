ae152de32e174c2b3bc88b6e32d68de1af06a836
Sebah7 <sebahismail21@gmail.com>
2024-09-24 19:09:59 +0200

    category main design - done

--- Fronts/src/Pages/WelcomePage.tsx
+++ Fronts/src/Pages/WelcomePage.tsx
@@ -1,9 +1,18 @@
+import Category from "../components/Welcome/Categories";
 import Hero from "../components/Welcome/Hero";
 
 const Welcome = () => {
   
   return (
+    <>
     <Hero />
+
+<div className="bg-clayAsh">
+    <Category title="Search by catergory"/>
+
+    <Category title="Search by criteria"/>
+</div>
+    </>
 );
   };
 
