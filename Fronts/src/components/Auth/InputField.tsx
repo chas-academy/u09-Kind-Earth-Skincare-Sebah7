a1c11116069e132d186eab450c098c7e1099b585
Sebah7 <sebahismail21@gmail.com>
2024-10-03 10:54:03 +0200

    contact us

--- Fronts/src/components/Auth/InputField.tsx
+++ Fronts/src/components/Auth/InputField.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 interface InputFieldProps {
-  label: string;
+  label?: string;
   type: string;
   value: string;
   id: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   autoComplete?: string;
   placeholder?: string;
+    isTextArea?: boolean;
 }
 
 const InputField: React.FC<InputFieldProps> = ({ 
@@ -18,9 +19,11 @@ const InputField: React.FC<InputFieldProps> = ({
   onChange,
   placeholder,
   autoComplete,
+    isTextArea = false,
 }) => {
   return (
       <div className="w-full mt-7">
+        {!isTextArea ? (
         <input
         type={type}
         value={value}
@@ -30,7 +33,16 @@ const InputField: React.FC<InputFieldProps> = ({
         autoComplete={autoComplete}
         className="font-sans appearance-none border
          border-black h-11 w-full max-w-full py-2 px-0 
-         text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+         text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+         />
+         ) : (
+        <textarea
+        value={value}
+          id={id}
+          placeholder={placeholder}
+          className="font-sans appearance-none border border-black h-32 w-full max-w-full py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        ></textarea>
+      )}
       </div>  
   );
 };
