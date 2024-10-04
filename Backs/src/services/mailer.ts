7a3c04fd31d3413f4499c9574b6267d817f33a40
Sebah7 <sebahismail21@gmail.com>
2024-10-04 11:27:11 +0200

    node mailer to handle the email connection

---
+++ Backs/src/services/mailer.ts
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async (to: string, subject: string, text: string) => {
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
