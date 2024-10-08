1416f6c9d2a655534b4221361432cf99a79de231
Sebah7 <sebahismail21@gmail.com>
2024-10-08 10:08:57 +0200

    renamed

--- Fronts/src/components/Admin/ProductForm.interface.ts
+++ Fronts/src/components/Admin/ProductForm.interface.ts
@@ -9,3 +9,9 @@ export interface ProductFormData {
   price: string;
   productImageUrl?: string | null;
 }
+
+// Interface for product fetched from the backend
+export interface Product extends ProductFormData {
+  _id: string; // Include MongoDB ID for the product
+  rating?: number; // Optional rating from your backend
+}
--- Fronts/src/components/Admin/ProductForm.tsx
+++ Fronts/src/components/Admin/ProductForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Button from "../Shared/Button";
-import { ProductFormData } from './ProdectForm.interface';
+import { ProductFormData } from './ProductForm.interface';
 
 const categoriesEnum = [
   "cleanser", "moisturizer", "serum", "toner", "mask",
