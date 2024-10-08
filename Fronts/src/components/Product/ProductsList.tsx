ad7645dec01b3326c5290499681fed040e620b4e
Sebah7 <sebahismail21@gmail.com>
2024-10-08 11:52:25 +0200

    All product fetched and listed

---
+++ Fronts/src/components/Product/ProductsList.tsx
@@ -0,0 +1,22 @@
+// ProductList.tsx
+import React from 'react';
+import { Product } from '../Admin/ProductForm.interface';
+import ProductItem from './ProductItem';
+
+interface ProductListProps {
+  products: Product[];
+}
+
+const ProductList: React.FC<ProductListProps> = ({ products }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+      {products.length === 0 ? (
+        <p>No products found.</p>
+      ) : (
+        products.map(product => <ProductItem key={product._id} product={product} />)
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
