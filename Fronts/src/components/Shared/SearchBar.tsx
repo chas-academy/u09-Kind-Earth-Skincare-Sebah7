bde86b96e6e8376644d87a5a6952b49bf988d591
Sebah7 <sebahismail21@gmail.com>
2024-10-11 11:12:19 +0200

    filter applied

--- Fronts/src/components/Shared/SearchBar.tsx
+++ Fronts/src/components/Shared/SearchBar.tsx
@@ -3,10 +3,13 @@ import { CiFilter, CiSearch} from "react-icons/ci";
 import { TiDelete } from "react-icons/ti";
 import { Product } from "../Admin/ProductForm.interface";
 import { useNavigate } from "react-router-dom";
+import FilterComponent from "../Product/ProductFilter";
 
 function SearchBar () {
 
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [filters, setFilters] = useState<{ [key: string]: string }>({})
+  const [showFilters, setShowFilters] = useState<boolean>(false);
   const navigate = useNavigate();
 
  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -15,10 +18,11 @@ function SearchBar () {
 
 const handleSubmit = async (event: React.FormEvent) => {
   event.preventDefault();
-  if (!searchQuery) return;
+  if (!searchQuery&& Object.keys(filters).length === 0) return;
 
   try {
     const queryParams = new URLSearchParams({
+            ...filters,
       name: searchQuery.toLowerCase(),
     }).toString();
 
@@ -36,6 +40,11 @@ const handleSubmit = async (event: React.FormEvent) => {
   
   const handleReset = () => {
     setSearchQuery("");
+    setFilters({});
+  };
+
+  const toggleFilters = () => {
+    setShowFilters((prevShowFilters) => !prevShowFilters);
   };
 
   return (
@@ -44,7 +53,8 @@ const handleSubmit = async (event: React.FormEvent) => {
     onSubmit={handleSubmit}
     className="flex flex-grow flex-wrap items-center justify-between gap-x-1.5 gap-y-1.5 rounded-[40px] border border-solid border-x-clayAsh border-y-clayAsh pb-1.5 pl-3 pr-[17px] pt-2 focus-within:[box-shadow:0_0_4px_#1e40af] min-[362px]:flex-nowrap"
     >
-      <button type="button" 
+      <button type="button"
+      onClick={toggleFilters}
        className="flex flex-col bg-transparent border-transparent transition-transform duration-200 ease-in-out hover:scale-110 cursor-pointer">
         <CiFilter className="text-formSecondaryText h-5 w-6" />
       </button>
@@ -68,6 +78,10 @@ const handleSubmit = async (event: React.FormEvent) => {
         <CiSearch type="submit" className="text-formSecondaryText h-5 w-6" />
       </button>
     </form>
+
+    {showFilters && (
+        <FilterComponent filters={filters} setFilters={setFilters} handleSubmit={handleSubmit}/>
+      )}
 
 </>
   );
