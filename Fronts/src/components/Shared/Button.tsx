f0d7c63143c79821c716578aad435b253f8ddf5e
Sebah7 <sebahismail21@gmail.com>
2024-10-17 13:21:46 +0200

    quiz btns.

--- Fronts/src/components/Shared/Button.tsx
+++ Fronts/src/components/Shared/Button.tsx
@@ -4,13 +4,15 @@ interface ButtonProps {
   text: string;
   onClick?: () => void;
   type?: "button" | "submit" | "reset"; 
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, type = "button", onClick }) => {
+const Button: React.FC<ButtonProps> = ({ text, type = "button", onClick, disabled }) => {
   return (
     <button
 type={type} 
-onClick={onClick}     
+onClick={onClick} 
+disabled={disabled}    
  className={`font-sans text-center text-[32px] leading-none 
   tracking-[0px] text-white flex w-full items-center justify-center 
   rounded-lg border-none
@@ -18,7 +20,7 @@ onClick={onClick}
   px-14 py-[7.3px] transition duration-200 
   ease-in-out hover:bg-formPrimaryText 
   hover:scale-105 active:scale-95 active:gb-neutral-700
-   aria-pressed="false"`}
+   ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
     >
       {text}
     </button>
