af2809ccef11231a0934261271f000409f1d1ba5
Sebah7 <sebahismail21@gmail.com>
2024-10-22 14:53:28 +0200

    div added

---
+++ Fronts/src/components/Dashboard/Tab/TabPanel.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel: React.FC<TabPanelProps> = ({ children, value, index, ...other }) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3, height: '100%' }}>
+          <Typography>
+            <div>{children}</div>
+            </Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export default TabPanel;
