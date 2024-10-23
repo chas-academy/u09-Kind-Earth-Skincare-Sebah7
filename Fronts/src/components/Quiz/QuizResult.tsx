2f040874c1511400f3564b1aca8909e22b06f325
Sebah7 <sebahismail21@gmail.com>
2024-10-23 12:54:50 +0200

    add routine to user

--- Fronts/src/components/Quiz/QuizResult.tsx
+++ Fronts/src/components/Quiz/QuizResult.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "../Shared/Button";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { PacmanLoader } from "react-spinners";
+import axiosInstance from "../../utils/axiosInstance";
 
 interface Product {
   name: string;
   category: string;
-}
+  _id: string;
+};
 
 interface RecommendationProps {
   mainConcern: string;
@@ -13,6 +18,48 @@ interface RecommendationProps {
 }
 
 const RecommendationComponent: React.FC<RecommendationProps> = ({ mainConcern, products, onAddToRoutine }) => {
+
+const { user , loading} = useAuth();
+const navigate = useNavigate();
+
+useEffect(() => {
+    const previousPage = localStorage.getItem("previousPage");
+    if (previousPage && user) {
+      localStorage.removeItem("previousPage");
+      onAddToRoutine();
+    }
+  }, [user, onAddToRoutine]);
+
+
+  const handleAddToRoutine = async (): Promise<void> => {
+    
+  if (!user) {
+    localStorage.setItem("previousPage", window.location.pathname);
+    navigate("/login");
+    return;
+  }
+  const productIds = products.map(product => product._id);
+  const userData = localStorage.getItem("userData");
+  const token = localStorage.getItem("authToken"); 
+  if (userData && token) {
+      const user = JSON.parse(userData);
+  try {
+    await axiosInstance.post("/users/saveroutine", { productIds },
+      { headers: 
+        { Authorization: `Bearer ${user.token}`} 
+      }
+    );
+    alert("Products added to routine!");
+  } catch (error) {
+    console.error("Error saving routine:", error);
+    alert("Failed to add products to routine.");
+  }
+}
+}
+
+ if (loading) {
+    return <PacmanLoader color="#91b553" />;
+  }
 
   return (
     <div className="p-4">
@@ -27,7 +74,8 @@ const RecommendationComponent: React.FC<RecommendationProps> = ({ mainConcern, p
       </ul>
 
     <div className="text-nowrap w-1/2">
-      < Button  onClick={onAddToRoutine} 
+      < Button  
+      onClick={handleAddToRoutine} 
       type="button" 
       text="Add to routine"
       />
