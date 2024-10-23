e7ce3363176f4881273ac68991201fa14851cdb6
Sebah7 <sebahismail21@gmail.com>
2024-10-23 14:29:24 +0200

    authed

--- Backs/src/routes/routineRoutes.ts
+++ Backs/src/routes/routineRoutes.ts
@@ -5,12 +5,13 @@ import {
   renameRoutine,
   routineMatchController,
 } from "../controllers/routineController";
+import auth from "../middlewares/authMiddleware";
 
 const RoutineRouter = Router();
 
 RoutineRouter.post("/routine-match", routineMatchController);
-RoutineRouter.get("/getRoutines", getRoutines);
-RoutineRouter.delete("/:routineId/delete", deleteRoutine);
-RoutineRouter.put("/:routineId/rename", renameRoutine);
+RoutineRouter.get("/getRoutines", auth, getRoutines);
+RoutineRouter.delete("/:routineId/delete", auth, deleteRoutine);
+RoutineRouter.put("/:routineId/rename", auth, renameRoutine);
 
 export default RoutineRouter;
