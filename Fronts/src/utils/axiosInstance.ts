158c34286879c27422a50113ee494902481279fc
Sebah7 <sebahismail21@gmail.com>
2024-10-23 14:32:52 +0200

    render

--- Fronts/src/utils/axiosInstance.ts
+++ Fronts/src/utils/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://u09-kind-earth-skincare-sebah7-4.onrender.com/api",
+  // "http://localhost:3000/api",
 });
 
 const getAuthToken = (): string | null => {
